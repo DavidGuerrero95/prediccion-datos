ec2b19d80d91414a6dbb0c8d7f4add0131474f7b
santiago <davids.guerrero@udea.edu.co>
2022-11-22 15:45:27 -0500

    Initial commit

---
+++ src/main/java/app/prediccion/predicciondatos/PrediccionDatosApplication.java
@@ -0,0 +1,13 @@
+package app.prediccion.predicciondatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrediccionDatosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrediccionDatosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/prediccion/predicciondatos/PrediccionDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package app.prediccion.predicciondatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrediccionDatosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
