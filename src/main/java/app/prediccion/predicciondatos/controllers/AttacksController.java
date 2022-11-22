f23fc39ce711929ad0ea2660d78e9081bdecbd35
santiago <davids.guerrero@udea.edu.co>
2022-11-22 18:48:39 -0500

    Añadir metodo para crear un csv

--- src/main/java/app/prediccion/predicciondatos/controllers/AttacksController.java
+++ src/main/java/app/prediccion/predicciondatos/controllers/AttacksController.java
@@ -1,11 +1,15 @@
 package app.prediccion.predicciondatos.controllers;
 
 import app.prediccion.predicciondatos.models.Attacks;
-import app.prediccion.predicciondatos.repository.AttacksRepository;
+import app.prediccion.predicciondatos.services.CSVService;
 import app.prediccion.predicciondatos.services.IAttacksService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,12 +22,29 @@ import java.util.List;
 @RequestMapping("/attacks")
 public class AttacksController {
 
-    @Autowired
-    IAttacksService attacksService;
+    private final IAttacksService attacksService;
+    private final CSVService fileService;
+
+    public AttacksController(IAttacksService attacksService, CSVService fileService) {
+        this.attacksService = attacksService;
+        this.fileService = fileService;
+    }
 
     @GetMapping("/list")
     @ResponseStatus(code = HttpStatus.OK)
     public List<Attacks> listAttacks(){
         return attacksService.attacksLists();
+    }
+
+    @GetMapping("/download")
+    @ResponseStatus(code = HttpStatus.OK)
+    public ResponseEntity<Resource> getFile() {
+        String filename = "attacks.csv";
+        InputStreamResource file = new InputStreamResource(fileService.load());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+                .contentType(MediaType.parseMediaType("application/csv"))
+                .body(file);
     }
 }
